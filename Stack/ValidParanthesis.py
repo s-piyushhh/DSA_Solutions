f615e1a2a8e68faa704f81e8563ef5871edc7a29
Piyush Shirbhayye <piyushshirbhayye@gmail.com>
2026-05-24 23:52:07 +0530

    Stack Problems No 1

---
+++ Stack/ValidParanthesis.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        for i in s:
+            if len(st) != 0:
+                if i == ")" and st[-1] == "(":
+                    st.pop()
+                elif i == "]" and st[-1] == "[":
+                    st.pop()
+                elif i == "}" and st[-1] == "{":
+                    st.pop()
+                else:
+                    st.append(i)
+            else:
+                st.append(i)
+
+        if len(st) == 0:
+            return True
+        return False
+    
+if __name__ == "__main__":
+    obj = Solution()
+
+    # s = "()"
+    s = "()[]{}"
+    # s = "(]"
+    # s = "([])"
+    # s = "([)]"            
+    
+    ans = obj.isValid(s)
+    
+    if ans:
+        print("Its Valid Paranthesis")
+    else:    
+        print("Its Not a Valid Paranthesis")
