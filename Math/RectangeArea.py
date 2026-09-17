904d7d8a7fef4145cc7488276287a0c46ee572bb
Piyush Shirbhayye <piyushshirbhayye@gmail.com>
2026-09-17 14:15:44 +0530

    17/09/26

---
+++ Math/RectangeArea.py
@@ -0,0 +1,9 @@
+class Solution:
+    def RectangleArea(self, x, y):
+        return x * y
+    
+if __name__ == "__main__":
+    solution = Solution()
+    x = 5
+    y = 10
+    print(solution.RectangleArea(x, y))  # Output: 50
